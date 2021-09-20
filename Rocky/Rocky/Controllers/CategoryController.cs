d49eeed9d9709005fdbdba1d453276d664e204ca
Saqib Razzaq <saqibrazzaq@gmail.com>
2021-09-20 21:25:57 +0500

    Category list, create and validation

---
+++ Rocky/Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // Get db instance
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+        // Default - Get all categories
+        public IActionResult Index()
+        {
+            // Get list of categories
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+
+        // GET - Create Form
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST - Create Form submit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add category in db
+                _db.Category.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+    }
+}
---
+++ Rocky/Rocky/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Data
+{
+    // It must be setup in startup.cs
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        // Tables
+        public DbSet<Category> Category { get; set; }
+    }
+}
