4d537adef166b5c6edb0b9e7be04086fc87d72b1
Saqib Razzaq <saqibrazzaq@gmail.com>
2021-09-21 23:25:41 +0500

    update and delete

--- Rocky/Rocky/Controllers/CategoryController.cs
+++ Rocky/Rocky/Controllers/CategoryController.cs
@@ -45,5 +45,67 @@ namespace Rocky.Controllers
             }
             return View(category);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null|| id == 0)
+            {
+                return NotFound();
+            }
+
+            // Get record from database
+            var category = _db.Category.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // Get - Delete
+        public IActionResult Delete(int? id)
+        {
+            // Find the id in database and pass it to delete confirmation page
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Category.Find(id);
+            // If id not found
+            if (category == null)
+            {
+                return NotFound();
+            }
+            // Pass category to the view
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category category)
+        {
+            if (category == null)
+            {
+                return NotFound();
+            }
+            // Delete from database
+            _db.Category.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
